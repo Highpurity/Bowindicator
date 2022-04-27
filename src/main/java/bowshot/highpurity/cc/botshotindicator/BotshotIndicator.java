8c741f7fd37385a7e712c29a5b6c05472f1c7f31
Sourceadmin <Robertaburdsall@gmail.com>
2022-04-26 20:08:14 -0400

    Starting Development Now

--- src/main/java/bowshot/highpurity/cc/botshotindicator/BotshotIndicator.java
+++ src/main/java/bowshot/highpurity/cc/botshotindicator/BotshotIndicator.java
@@ -1,17 +1,21 @@
 package bowshot.highpurity.cc.botshotindicator;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BotshotIndicator extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+       System.out.print(Color.GREEN + "BowshotIndicator is Initializing...");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.print(Color.RED + "BowshotIndicator is now Disabling...");
+
     }
 }
